e655ec8ff7fb5c5b597918dbcb78326d295cda42
unknown <ana.nalbandyan@gmail.com>
2021-03-03 09:58:50 -0800

    setting up classes

---
+++ SweepstakesQueueManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class SweepstakesQueueManager : ISweepstakesManager
+    {
+        //uses Queue data structure as an underlying structure
+
+        void ISweepstakesManager.InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            throw new NotImplementedException();
+        }
+
+        Sweepstakes ISweepstakesManager.GetSweepstakes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SweepstakesStackManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepStakes
+{
+    class SweepstakesStackManager : ISweepstakesManager
+    {
+        //uses Stack data as an underlying structure
+
+        void ISweepstakesManager.InsertSweepstakes(Sweepstakes sweepstakes)
+        {
+            throw new NotImplementedException();
+        }
+
+        Sweepstakes ISweepstakesManager.GetSweepstakes()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
